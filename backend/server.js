606920334ca6d5ac9d0ab14eeb97e926c83bbcb2
Juliet DiMenna <jdimenna@bayviewglen.ca>
2025-03-28 13:56:46 -0400

    auth

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js';
+import protectedRoutes from './routes/protected.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI);
+
+app.use('/api', authRoutes);
+app.use('/api', protectedRoutes);
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
